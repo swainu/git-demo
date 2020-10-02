97a821295056e5ff3cf2e9f0abfcaa82109c3269
udaya <udswain@gmail.com>
2020-10-02 09:40:22 +0530

    added new mthod app.java

--- src/main/java/com/git/App.java
+++ src/main/java/com/git/App.java
@@ -4,10 +4,12 @@ package com.git;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+	public String test() {
+		return "test";
+	}
 }
